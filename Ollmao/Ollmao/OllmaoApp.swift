a94a20e31caf8060ca7de5d6f2e02ded181e6c76
Zigao Wang <zigao.wang24@gmail.com>
2025-01-28 15:34:48 +0800

    feat: set up macos app in xcode

---
+++ Ollmao/Ollmao/OllmaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OllmaoApp.swift
+//  Ollmao
+//
+//  Created by Zigao Wang on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct OllmaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
